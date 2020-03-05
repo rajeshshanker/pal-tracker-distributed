7447f69d6781a42c644b95740d132b43f49b39a8
Rajesh Shanker <rajesh.bms@gmail.com>
2020-03-05 14:29:23 +0530

    Updated bootstrap props

--- components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
+++ components/allocations/src/main/java/io/pivotal/pal/tracker/allocations/ProjectClient.java
@@ -21,14 +21,23 @@ public class ProjectClient {
     }
     @HystrixCommand(fallbackMethod = "getProjectFromCache")
     public ProjectInfo getProject(long projectId) {
+        ProjectInfo project = null;
         logger.info("Getting project with id {} from DB", projectId) ;
-        ProjectInfo project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+        try {
+             project = restOperations.getForObject(endpoint + "/projects/" + projectId, ProjectInfo.class);
+            logger.info("After retrival");
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
         projectsCache.put(projectId, project);
+        logger.info("Displaying List of projects available in DB") ;
+        projectsCache.forEach((projectIdKey, projectname) -> logger.info((projectIdKey + ":" + projectname)));
         return project;
     }
 
     public ProjectInfo getProjectFromCache(long projectId) {
         logger.info("Getting project with id {} from cache", projectId);
+        logger.info("Displaying List of projects available in DB") ;
         return projectsCache.get(projectId);
     }
 }
